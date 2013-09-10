e7381887170894ea234c13c444cdcc735cfa02c7
Patrick Hebron <patchiehebron@gmail.com>
2013-09-09 23:15:15 -0400

    Adding types, union and struct examples

---
+++ applications/examples/DataStructures01/DataStructures01/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <limits>
+#include <string>
+
+using namespace std;
+
+/** @brief An example union of numeric data types */
+union NumericUnion
+{
+	bool			mBool;
+	char			mChar;
+	unsigned int	mUint;
+	int				mInt;
+	long			mLong;
+	float			mFloat;
+	double			mDouble;
+};
+
+/** @brief An example union of numeric and string data types */
+union GenericUnion
+{
+	bool			mBool;
+	char			mChar;
+	unsigned int	mUint;
+	int				mInt;
+	long			mLong;
+	float			mFloat;
+	double			mDouble;
+	const char*		mCstring;
+	std::string		mString;
+};
+
+/** @brief An example struct containing numeric data types */
+struct NumericStruct
+{
+	bool			mBool;
+	char			mChar;
+	unsigned int	mUint;
+	int				mInt;
+	long			mLong;
+	float			mFloat;
+	double			mDouble;
+};
+
+/** @brief An example struct containing numeric and string data types */
+struct GenericStruct
+{
+	bool			mBool;
+	char			mChar;
+	unsigned int	mUint;
+	int				mInt;
+	long			mLong;
+	float			mFloat;
+	double			mDouble;
+	const char*		mCstring;
+	std::string		mString;
+};
+
+
+int main(int argc, const char * argv[])
+{
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "TYPES:" << endl << endl << endl;
+	
+	cout << "bool:" << endl;
+	cout << "\tSize: " << sizeof( bool ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << numeric_limits<bool>::min() << ", " << numeric_limits<bool>::max() << " )" << endl << endl;
+	
+	cout << "char:" << endl;
+	cout << "\tSize: " << sizeof( char ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << (int)numeric_limits<char>::min() << ", " << (int)numeric_limits<char>::max() << " )" << endl << endl;
+	
+	cout << "unsigned int:" << endl;
+	cout << "\tSize: " << sizeof( unsigned int ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << numeric_limits<unsigned int>::min() << ", " << numeric_limits<unsigned int>::max() << " )" << endl << endl;
+	
+	cout << "int:" << endl;
+	cout << "\tSize: " << sizeof( int ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << numeric_limits<int>::min() << ", " << numeric_limits<int>::max() << " )" << endl << endl;
+	
+	cout << "long:" << endl;
+	cout << "\tSize: " << sizeof( long ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << numeric_limits<long>::min() << ", " << numeric_limits<long>::max() << " )" << endl << endl;
+	
+	cout << "float:" << endl;
+	cout << "\tSize: " << sizeof( float ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << numeric_limits<float>::min() << ", " << numeric_limits<float>::max() << " )" << endl << endl;
+	
+	cout << "double:" << endl;
+	cout << "\tSize: " << sizeof( double ) << " byte(s)" << endl;
+	cout << "\tRange: ( " << numeric_limits<double>::min() << ", " << numeric_limits<double>::max() << " )" << endl << endl;
+	
+	cout << "const char*:" << endl;
+	cout << "\tSize: " << sizeof( const char* ) << " byte(s)" << endl;
+	cout << "\tRange: N/A" << endl << endl;
+	
+	cout << "std::string:" << endl;
+	cout << "\tSize: " << sizeof( std::string ) << " byte(s)" << endl;
+	cout << "\tRange: N/A" << endl << endl;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "UNIONS AND STRUCTS:" << endl << endl << endl;
+	
+	cout << "NumericUnion:" << endl;
+	cout << "\tSize: " << sizeof( NumericUnion ) << " byte(s)" << endl << endl;
+	
+	cout << "GenericUnion:" << endl;
+	cout << "\tSize: " << sizeof( GenericUnion ) << " byte(s)" << endl << endl;
+	
+	cout << "NumericStruct:" << endl;
+	cout << "\tSize: " << sizeof( NumericStruct ) << " byte(s)" << endl << endl;
+	
+	cout << "GenericStruct:" << endl;
+	cout << "\tSize: " << sizeof( GenericStruct ) << " byte(s)" << endl << endl;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "ARRAYS:" << endl << endl << endl;
+	
+	int arrayA[10];
+	int arrayB[1000];
+	
+	cout << "int arrayA[10]:" << endl;
+	cout << "\tSize: " << sizeof( arrayA ) << " byte(s)" << endl;
+	cout << "\tLength: " << ( sizeof( arrayA ) / sizeof( int ) ) << endl << endl;
+	
+	cout << "int arrayB[1000]:" << endl;
+	cout << "\tSize: " << sizeof( arrayB ) << " byte(s)" << endl;
+	cout << "\tLength: " << ( sizeof( arrayB ) / sizeof( int ) ) << endl << endl;
+	
+	int* arrayC = new int[10];
+	int* arrayD = new int[1000];
+	
+	cout << "int* arrayC = new int[10]:" << endl;
+	cout << "\tSize: " << sizeof( arrayC ) << " byte(s)" << endl;
+	cout << "\tLength: Cannot be inferred from pointer" << endl << endl;
+	
+	cout << "int* arrayD = new int[1000]:" << endl;
+	cout << "\tSize: " << sizeof( arrayD ) << " byte(s)" << endl;
+	cout << "\tLength: Cannot be inferred from pointer" << endl << endl;
+	
+	delete [] arrayC;
+	delete [] arrayD;
+	
+    return 0;
+}
+
---
+++ applications/examples/DataStructures02/DataStructures02/main.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/** @brief An example union of numeric data types */
+union NumericUnion
+{
+	bool	mBool;
+	int		mInt;
+	float	mFloat;
+};
+
+/** @brief An example struct containing numeric data types */
+struct NumericStruct
+{
+	bool	mBool;
+	int		mInt;
+	float	mFloat;
+};
+
+/** @brief An enumeration of supported numeric types */
+enum NumericType
+{
+	T_NULL, T_BOOL, T_INT, T_FLOAT
+};
+
+/** @brief An example class containing a union of numeric data types */
+class NumericUnionClass
+{
+public:
+	/** @brief Default constructor */
+	NumericUnionClass() : mType(T_NULL) { /* no-op */ }
+	/** @brief Empty destructor */
+	~NumericUnionClass() { /* no-op */ }
+	
+	const NumericType& getType() const
+	{
+		return mType;
+	}
+	
+	bool isBool() const
+	{
+		return ( mType == T_BOOL );
+	}
+	
+	bool isInt() const
+	{
+		return ( mType == T_INT );
+	}
+	
+	bool isFloat() const
+	{
+		return ( mType == T_FLOAT );
+	}
+	
+	bool& getBool()
+	{
+		return mUnion.mBool;
+	}
+	
+	int& getInt()
+	{
+		return mUnion.mInt;
+	}
+	
+	float& getFloat()
+	{
+		return mUnion.mFloat;
+	}
+	
+	void setBool(const bool& iBool)
+	{
+		mUnion.mBool = iBool;
+		mType = T_BOOL;
+	}
+	
+	void setInt(const int& iInt)
+	{
+		mUnion.mInt = iInt;
+		mType = T_INT;
+	}
+	
+	void setFloat(const float& iFloat)
+	{
+		mUnion.mFloat = iFloat;
+		mType = T_FLOAT;
+	}
+	
+private:
+	NumericType		mType;	//!< The type currently being stored
+	NumericUnion	mUnion; //!< The internal copy of the union
+};
+
+int main(int argc, const char * argv[])
+{
+	NumericUnion tUnion;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "SET: tUnion.mInt = 123;" << endl << endl << endl;
+	tUnion.mInt   = 123;
+	
+	cout << "GET:" << endl;
+	cout << "tUnion.mBool   = " << tUnion.mBool << endl;
+	cout << "tUnion.mInt    = " << tUnion.mInt << endl;
+	cout << "tUnion.mFloat  = " << tUnion.mFloat << endl;
+	
+	cout << endl << endl;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "SET: tUnion.mFloat = 456.7;" << endl << endl << endl;
+	tUnion.mFloat = 456.7;
+	
+	cout << "GET:" << endl;
+	cout << "tUnion.mBool   = " << tUnion.mBool << endl;
+	cout << "tUnion.mInt    = " << tUnion.mInt << endl;
+	cout << "tUnion.mFloat  = " << tUnion.mFloat << endl;
+	
+	NumericStruct tStruct;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "SET: tStruct.mInt = 123;" << endl << endl << endl;
+	tStruct.mInt   = 123;
+	
+	cout << "GET:" << endl;
+	cout << "tStruct.mBool   = " << tStruct.mBool << endl;
+	cout << "tStruct.mInt    = " << tStruct.mInt << endl;
+	cout << "tStruct.mFloat  = " << tStruct.mFloat << endl;
+	
+	cout << endl << endl;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "SET: tStruct.mFloat = 456.7;" << endl << endl << endl;
+	tStruct.mFloat = 456.7;
+	
+	cout << "GET:" << endl;
+	cout << "tStruct.mBool   = " << tStruct.mBool << endl;
+	cout << "tStruct.mInt    = " << tStruct.mInt << endl;
+	cout << "tStruct.mFloat  = " << tStruct.mFloat << endl;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "NumericUnionClass:" << endl << endl;
+	
+	NumericUnionClass tUnionClass = NumericUnionClass();
+	
+	tUnionClass.setFloat( 123.4 );
+	
+	if( tUnionClass.isBool() ) {
+		cout << "tUnionClass.mUnion.mBool = " << tUnionClass.getBool() << endl;
+	}
+	else {
+		cout << "tUnionClass does not contain a bool!" << endl;
+	}
+	
+	if( tUnionClass.isInt() ) {
+		cout << "tUnionClass.mUnion.mInt = " << tUnionClass.getInt() << endl;
+	}
+	else {
+		cout << "tUnionClass does not contain an int!" << endl;
+	}
+	
+	if( tUnionClass.isFloat() ) {
+		cout << "tUnionClass.mUnion.mFloat = " << tUnionClass.getFloat() << endl;
+	}
+	else {
+		cout << "tUnionClass does not contain a float!" << endl;
+	}
+	
+	cout << endl << endl;
+	
+	return 0;
+}
+
+
